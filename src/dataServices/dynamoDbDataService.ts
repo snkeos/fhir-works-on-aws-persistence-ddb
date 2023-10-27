f9717a4f4c1d056c615fc4d34b4b9bbf90a0e90d
Yevhen Onykiienko <yevhen.onykiienko@snkeos.com>
2023-10-27 16:04:39 +0200

    add debug message

--- src/dataServices/dynamoDbDataService.ts
+++ src/dataServices/dynamoDbDataService.ts
@@ -195,6 +195,8 @@ export class DynamoDbDataService implements Persistence, BulkDataAccess {
 
     async deleteVersionedResource(id: string, vid: number, tenantId?: string) {
         const deleteParamInput = DynamoDbParamBuilder.buildDeleteParam(id, vid, tenantId).Delete;
+        const dParams = JSON.stringify(deleteParamInput);
+        console.error(`>>> Delete Item with the params: ${dParams}`)
         await this.dynamoDb.deleteItem(deleteParamInput).promise();
         return {
             success: true,
